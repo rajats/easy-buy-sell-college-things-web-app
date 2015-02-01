5a55db6c00a39f558b943b055748c7412cf15be2
Rajat <rajat_kumar@outlook.com>
2015-02-02 00:39:14 +0530

    initial commit

---
+++ market/settings.py
@@ -0,0 +1,130 @@
+"""
+Django settings for market project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '$g(m-u_v+=0vus1a#jdegez_!2zksgsh576i3#oijt9$z13qa3'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'south',
+    #'registration',
+    'products',
+    'profiles',
+    'cart',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'market.urls'
+
+WSGI_APPLICATION = 'market.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',                                                                                                                                                                       
+        'NAME': 'easybuydb',
+        'USER': 'rajat',                      # Not used with sqlite3.
+        'PASSWORD': 'rajat711',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'Asia/Kolkata'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = 'C:/Python27/django16/market/static/media/'
+
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+
+#PROTECTED_UPLOAD = 'C:/Python27/django16/market/static/protected/'
+
+MEDIA_URL = '/static/media/'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/C:/Python27/django16/market/static/media/media.lawrence.com/static/"
+STATIC_ROOT = 'C:/Python27/django16/market/static/static-files'
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    ('C:/Python27/django16/market/static/'),
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+TEMPLATE_DIRS = (
+    'C:/Python27/django16/market/static/templates/',
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
---
+++ market/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings 
+from django.conf.urls.static import static
+from django.contrib import admin
+admin.autodiscover()
+
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'esell.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    #without regular expression you cant use css,javascript..
+    url(r'^$','market.views.home', name="home"),           #this home page url
+    url(r'^lib/','profiles.views.library', name="library"),
+    (r'^products/', include('products.urls')),
+    (r'^cart/', include('cart.urls')),
+    (r'^userauth/', include('userauth.urls')),
+    #url(r'^products/(?P<slug>.*)/$', 'products.views.single', name="single"),
+    (r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root: settings.STATIC_ROOT'}),
+    (r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root: settings.MEDIA_ROOT'}),
+    url(r'^admin/', include(admin.site.urls)),
+)
