792f5df9c64977c53aa24bf8ae17ecf5d16545dc
Rajat <rajat_kumar@outlook.com>
2015-02-18 01:26:31 +0530

    using ajax within ajax to mark all notifications as read

--- notifications/urls.py
+++ notifications/urls.py
@@ -6,4 +6,5 @@ from django.conf import settings
 urlpatterns = patterns('notifications.views',
     url(r'^$','all', name="notifications_all"),
     url(r'^ajax/$', 'get_notifications_using_ajax', name='get_notifications_using_ajax'),
+    url(r'^markallread/$', 'mark_all_as_read', name='mark_all_as_read'),
 )
--- notifications/views.py
+++ notifications/views.py
@@ -19,13 +19,32 @@ def all(request):
 def get_notifications_using_ajax(request):                                       #return HttpResponse with ajax data
 	if request.is_ajax() and request.method == "POST":
 		notifications = Notification.objects.all_for_user(request.user)
-		count = notifications.count()
+		count = 0
 		notes = []
 		for note in notifications:
-			notes.append(str(note.get_link()))
+			if not note.read:
+				notes.append(str(note.get_link()))
+				count += 1
 		data = {
 			"notifications": notes,
 			"count": count,
+		}
+		json_data = json.dumps(data)               #converts to json 
+		return HttpResponse(json_data, content_type='application/json')
+	else:
+		raise Http404
+
+@login_required
+def mark_all_as_read(request):                                       #return HttpResponse with ajax data
+	if request.is_ajax() and request.method == "POST":
+		notifications = Notification.objects.all_for_user(request.user)
+		notes = []
+		test=2
+		for note in notifications:
+			note.read=True
+			note.save()
+		data = {
+			"notifications": notes,
 		}
 		json_data = json.dumps(data)               #converts to json 
 		return HttpResponse(json_data, content_type='application/json')
